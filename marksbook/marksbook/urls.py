c69052a6c0839f44aa93bdd7167e1085b246e99e
Alexandr Trifonov <lordsanchez@yandex.ru>
2023-08-16 17:35:21 +0400

    readme & swagger nginx

--- marksbook/marksbook/urls.py
+++ marksbook/marksbook/urls.py
@@ -1,18 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path, re_path
-from django.views.generic import TemplateView
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls', namespace='api')),
-    path(
-        'redoc/',
-        TemplateView.as_view(template_name='redoc.html'),
-        name='redoc'
-    ),
+    path('api/', include('api.urls', namespace='api'))
 ]
 
 
